43f982f290aa35df19bf355d3571742848f35dab
Evan Turner <evturn@gmail.com>
2015-01-26 21:27:02 -0500

    mocha tests work with querystring test=1

--- meadowlark.js
+++ meadowlark.js
@@ -10,7 +10,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 app.use(function(req, res, next) {
-	res.locals.showTests = app.get('env') != 'production' && req.query.test === '1';
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
 	next();
 });
 
