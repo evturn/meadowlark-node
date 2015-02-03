181dca9202bda97a100bdb5da506e52a144c3650
Evan Turner <evturn@gmail.com>
2015-02-03 15:14:24 -0500

    added route handlers for uploading files

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var fortune = require('./lib/fortune.js');
+var express 	 = require('express');
+var fortune 	 = require('./lib/fortune.js');
+var formidable = require('formidable');
 
 var app = express();
 
@@ -22,6 +23,26 @@ app.use(function(req, res, next) {
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
 	next();
 });
+
+app.get('/.contest/vacation-photo', function(req, res) {
+	var now = new Date();
+	res.render('contest/vacation-photo', {
+		year: now.getFullYear(), month: now.getMont()
+	});
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+		if (err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files:');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
+});
+
 
 app.get('/newsletter', function(req, res) {
 	res.render('newsletter', {csrf: 'CSRF token goes here'});
