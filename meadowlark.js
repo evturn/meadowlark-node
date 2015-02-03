35b5a12368ccb36e8a2c72926303b205ea08a8c1
Evan Turner <evturn@gmail.com>
2015-02-03 15:42:27 -0500

    form upload successfull but no redirect

--- meadowlark.js
+++ meadowlark.js
@@ -24,11 +24,11 @@ app.use(function(req, res, next) {
 	next();
 });
 
-app.get('/contest/vacation-photo', function(req, res) {
+app.get('/contest/vacation-photo',function(req,res) {
 	var now = new Date();
 	res.render('contest/vacation-photo', {
-		year: now.getFullYear(), month: now.getMonth()
-	});
+		year: now.getFullYear(),month: now.getMonth()
+  });
 });
 
 app.post('/contest/vacation-photo/:year/:month', function(req, res) {
@@ -39,8 +39,8 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res) {
 		console.log(fields);
 		console.log('received files:');
 		console.log(files);
-		res.redirect(303, '/thank-you');
-	});
+    res.redirect(303, '/thank-you');
+  });
 });
 
 
