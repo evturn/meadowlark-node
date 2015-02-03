604d39c7041c27aaef4b9c2b90ed15cd32926cc9
Evan Turner <evturn@gmail.com>
2015-02-02 21:33:37 -0500

    modified process POST route for error handling

--- meadowlark.js
+++ meadowlark.js
@@ -28,11 +28,11 @@ app.get('/newsletter', function(req, res) {
 });
 
 app.post('/process', function(req, res){
-	console.log('Form (from querystring): ' + req.query.form);
-	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-	console.log('Name (from visible form field): ' + req.body.name);
-	console.log('Email (from visible form field): ' + req.body.email);
-	res.redirect(303, '/thank-you');
+	if (req.xhr || req.accepts('json,html')==='json') {
+		res.send({success: true});
+	} else {
+  res.redirect(303, '/thank-you');
+  }
 });
 
 app.get('/tours/hood-river', function(req, res) {
